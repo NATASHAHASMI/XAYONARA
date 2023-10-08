6b709eb6b9498194a3dfb0b3a0118cde02433253
Xayonara7 <146075821+Xayonara7@users.noreply.github.com>
2023-10-08 15:35:47 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -81,9 +81,6 @@ async def start(client, message):
         await m.edit_text("<b><i>ꜱᴛᴀʀᴛɪɴɢ...</i></b>")
         await asyncio.sleep(0.4)
         await m.delete()
-        m=await message.reply_sticker("CAACAgQAAxkBAAEKG-Nk50URpkgyBkQ2iBpUVnnZqC7GpwACbg8AAuHqsVDaMQeY6CcRojAE")
-        await asyncio.sleep(0.8)
-        await m.delete()
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, gtxt, temp.U_NAME, temp.B_NAME),
