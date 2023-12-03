0fa0157317ae0a8911b2e95d2b4ca03411a29b3b
NATASHAHASMI <150996197+NATASHAHASMI@users.noreply.github.com>
2023-12-03 16:56:29 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2055,14 +2055,11 @@ async def auto_filter(client, msg, spoll=False):
                     return await advantage_spell_chok(client, msg)
                 else:
                     if NO_RESULTS_MSG:
-                        buttons = [[
-                                    InlineKeyboardButton("✅ DONE ✅", callback_data="close_data")
-                                  ]]
                         await client.send_message(
                             chat_id=LOG_CHANNEL,
                             text=script.NORSLTS.format(reqstr.id, reqstr.mention, search),
-                            reply_markup = InlineKeyboardMarkup(buttons)
-                        )
+                            await query.edit_message_reply_markup(
+                                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("✅ DONE ✅", callback_data="close_data")]]
                     return
 
         else:
