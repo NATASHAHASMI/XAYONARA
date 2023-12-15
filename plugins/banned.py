c76157aa1f79a1e395eda1cc970ec083bda0f99e
Tanujairam <88611197+Tanujairam-TG@users.noreply.github.com>
2023-12-15 13:31:23 +0530

    Update banned.py

--- plugins/banned.py
+++ plugins/banned.py
@@ -1,22 +1,18 @@
 from pyrogram import Client, filters
 from utils import temp
-from pyrogram.types import Message
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 from database.users_chats_db import db
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from info import SUPPORT_CHAT
 
 async def banned_users(_, client, message: Message):
-    return (
-        message.from_user is not None or not message.sender_chat
-    ) and message.from_user.id in temp.BANNED_USERS
+    return message.from_user is not None and not message.sender_chat and message.from_user.id in temp.BANNED_USERS
 
 banned_user = filters.create(banned_users)
 
 async def disabled_chat(_, client, message: Message):
     return message.chat.id in temp.BANNED_CHATS
 
-disabled_group=filters.create(disabled_chat)
-
+disabled_group = filters.create(disabled_chat)
 
 @Client.on_message(filters.private & banned_user & filters.incoming)
 async def ban_reply(bot, message):
@@ -25,16 +21,20 @@ async def ban_reply(bot, message):
 
 @Client.on_message(filters.group & disabled_group & filters.incoming)
 async def grp_bd(bot, message):
-    buttons = [[
-        InlineKeyboardButton('Support', url=f'https://t.me/{SUPPORT_CHAT}')
-    ]]
-    reply_markup=InlineKeyboardMarkup(buttons)
+    buttons = [
+        [InlineKeyboardButton('Support', url=f'https://t.me/{SUPPORT_CHAT}')]
+    ]
+    reply_markup = InlineKeyboardMarkup(buttons)
     vazha = await db.get_chat(message.chat.id)
-    k = await message.reply(
-        text=f"CHAT NOT ALLOWED 🐞\n\nMy admins has restricted me from working here ! If you want to know more about it contact support..\nReason : <code>{vazha['reason']}</code>.",
-        reply_markup=reply_markup)
+    reply_text = (
+        f"CHAT NOT ALLOWED 🐞\n\nMy admins have restricted me from working here! If you want to know more about it, contact support..\n"
+        f"Reason: <code>{vazha['reason']}</code>."
+    )
+    k = await message.reply(text=reply_text, reply_markup=reply_markup)
+    
     try:
         await k.pin()
-    except:
+    except Exception:
         pass
+
     await bot.leave_chat(message.chat.id)
