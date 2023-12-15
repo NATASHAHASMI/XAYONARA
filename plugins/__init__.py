f96e3f0fe94497d96ce22f06db5174a62084dc76
Tanujairam <88611197+Tanujairam-TG@users.noreply.github.com>
2023-12-15 12:30:53 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -6,3 +6,8 @@ async def web_server():
     web_app = web.Application(client_max_size=30000000)
     web_app.add_routes(routes)
     return web_app
+
+import time
+
+StartTime = time.time()
+__version__ = 1.1
