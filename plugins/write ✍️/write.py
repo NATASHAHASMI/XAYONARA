4d38c6efc13129d0b4c83b674bb734ec553211ff
NATASHAHASMI <150996197+NATASHAHASMI@users.noreply.github.com>
2024-01-26 22:12:52 +0530

    Update write.py

--- plugins/write ✍️/write.py
+++ plugins/write ✍️/write.py
@@ -10,9 +10,11 @@ from utils import temp
 async def handwrite(_, message: Message):
     if not message.reply_to_message:
         text = message.text.split(None, 1)[1]
-        m = await message.reply_text("<b>ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ...</b>"),
+        print(f"Text to write: {text}")  # Print the text to be written
+        m = await message.reply_text("<b>ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ...</b>")
         API = f"https://api.sdbots.tk/write?text={text}"
         req = requests.get(API).url
+        print(f"Generated URL: {req}")  # Print the generated URL
         await message.reply_photo(
             photo=req,
             caption=(MALIK.format(message.from_user.mention, temp.U_NAME, temp.B_NAME, message.chat.title, req)),
@@ -22,7 +24,4 @@ async def handwrite(_, message: Message):
         )
         await asyncio.sleep(0.3)
         await m.delete()
-
-MALIK = """<b>sᴜᴄᴄᴇssꜰᴜʟʟʏ ᴡʀɪᴛᴛᴇɴ ᴛᴇxᴛ ✔️\n\n🔊 ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ - {}.\n✍ ᴡʀɪᴛᴛᴇɴ ʙʏ - <a href=https://t.me/{}>{}</a>,\n👥 ɢʀᴏᴜᴘ - {}\n🖇 ʟɪɴᴋ - {}</b>"""
-
-MALIKK = """<b>ʜᴇʏ {}.\n\nᴘʟᴇᴀsᴇ ᴡᴀɪᴛ...\n\nᴡʀɪᴛɪɴɢ ʏᴏᴜʀ ᴛᴇxᴛ...</b>"""
+        
