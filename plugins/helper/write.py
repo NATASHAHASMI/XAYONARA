6690f22f4b2bfab78c3d72eb12d142d39e6fc952
NATASHAHASMI <150996197+NATASHAHASMI@users.noreply.github.com>
2024-01-27 16:51:24 +0530

    Update write.py

--- plugins/helper/write.py
+++ plugins/helper/write.py
@@ -10,11 +10,9 @@ from utils import temp
 async def handwrite(_, message: Message):
     if not message.reply_to_message:
         text = message.text.split(None, 1)[1]
-        print(f"Text to write: {text}")  # Print the text to be written
         m = await message.reply_text("<b>ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ...</b>")
-        API = f"https://api.sdbots.tk/write?text={text}"
+        API = f"https://api.sdbots.tech/write?text={text}"
         req = requests.get(API).url
-        print(f"Generated URL: {req}")  # Print the generated URL
         await message.reply_photo(
             photo=req,
             caption=(MALIK.format(message.from_user.mention, temp.U_NAME, temp.B_NAME, message.chat.title, req)),
