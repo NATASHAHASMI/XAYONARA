f92216909dcb2c2d5f99dc8443a0340faafc2e3b
NATASHAHASMI <150996197+NATASHAHASMI@users.noreply.github.com>
2024-01-29 15:04:22 +0530

    Update write.py

--- plugins/helper/write.py
+++ plugins/helper/write.py
@@ -5,9 +5,15 @@ from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from utils import temp
 
+# Add the owner's user ID here
+owner_id = 1843754190
 
 @Client.on_message(filters.command("write"))
 async def handwrite(_, message: Message):
+    if message.from_user.id != owner_id:
+        await message.reply_text("ᴘʟᴇᴀꜱᴇ ꜱᴇɴᴅ ᴛʜᴇ ᴛᴇxᴛ ɪɴ ᴛʜᴇ ᴄᴏʀʀᴇᴄᴛ ꜰᴏʀᴍᴀᴛ : /write ʏᴏᴜʀ ᴛᴇxᴛ ✍️")
+        return
+
     if not message.reply_to_message:
         text = message.text.split(None, 1)[1]
         m = await message.reply_text("<b>ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ...</b>")
@@ -23,7 +29,7 @@ async def handwrite(_, message: Message):
         await asyncio.sleep(0.3)
         await m.delete()
 
-
 MALIK = """<b>sᴜᴄᴄᴇssꜰᴜʟʟʏ ᴡʀɪᴛᴛᴇɴ ᴛᴇxᴛ ✔️\n\n🔊 ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ - {}.\n✍ ᴡʀɪᴛᴛᴇɴ ʙʏ - <a href=https://t.me/{}>{}</a>,\n👥 ɢʀᴏᴜᴘ - {}\n🖇 ʟɪɴᴋ - {}</b>"""
 
 MALIKK = """<b>ʜᴇʏ {}.\n\nᴘʟᴇᴀsᴇ ᴡᴀɪᴛ...\n\nᴡʀɪᴛɪɴɢ ʏᴏᴜʀ ᴛᴇxᴛ...</b>"""
+        
