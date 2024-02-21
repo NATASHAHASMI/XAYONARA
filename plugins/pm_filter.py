2e618be757aea7c83f71d79a046dbf62eb3989e3
NATASHAHASMI <150996197+NATASHAHASMI@users.noreply.github.com>
2024-02-21 13:55:32 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -89,7 +89,7 @@ async def pm_text(bot, message):
         text=f"<b>#𝐏𝐌_𝐌𝐒𝐆\n\nNᴀᴍᴇ : {user}\n\nID : {user_id}\n\nMᴇssᴀɢᴇ : {content}</b>"
     )
 
-@Client.on_message(filters.private & filters.text & filters.incoming & filters.group & filters.commands)
+@Client.on_message(filters.private & filters.text & filters.incoming & filters.group & filters.command)
 async def react_msg(client, message):
     emojis = [
         "👍",
