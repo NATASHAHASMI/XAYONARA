65e210d858ec68f6d5fbcd90fcfedf29ef7b57a3
NATASHAHASMI <150996197+NATASHAHASMI@users.noreply.github.com>
2024-07-04 02:52:49 +0530

    Update connections_mdb.py

--- database/connections_mdb.py
+++ database/connections_mdb.py
@@ -1,6 +1,6 @@
 import pymongo
 from sample_info import tempDict
-from info import DATABASE_URI, DATABASE_NAME
+from info import DATABASE_URI, DATABASE_NAME, SECONDDB_URI
 
 import logging
 logger = logging.getLogger(__name__)
