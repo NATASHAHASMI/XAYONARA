c5c4b9a6ba84b625c1181c3a2da4555e3b3afb0f
NATASHAHASMI <150996197+NATASHAHASMI@users.noreply.github.com>
2024-07-04 03:41:12 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -60,7 +60,7 @@ async def Lazy_start():
             load = importlib.util.module_from_spec(spec)
             spec.loader.exec_module(load)
             sys.modules["plugins." + plugin_name] = load
-            print("Xayon Imported => " + plugin_name)
+            print("Lazy Imported => " + plugin_name)
     if ON_HEROKU:
         asyncio.create_task(ping_server())
     b_users, b_chats = await db.get_banned()
