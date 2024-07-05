76a22f4102fa12e85d0c1bf875c8d7cff052246b
NATASHAHASMI <150996197+NATASHAHASMI@users.noreply.github.com>
2024-07-05 16:07:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-
 import sys
 import glob
 import importlib
@@ -19,12 +18,11 @@ logging.basicConfig(
 logging.getLogger("aiohttp").setLevel(logging.ERROR)
 logging.getLogger("aiohttp.web").setLevel(logging.ERROR)
 
-
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
-from database.ia_filterdb import Media
+from database.ia_filterdb import Media, Media2, choose_mediaDB, db as clientDB
 from database.users_chats_db import db
-from info import *
+from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR, LOG_CHANNEL, PORT, SECONDDB_URI
 from utils import temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
@@ -39,13 +37,12 @@ from pyrogram import idle
 from lazybot import LazyPrincessBot
 from util.keepalive import ping_server
 from lazybot.clients import initialize_clients
-
+from sample_info import tempDict
 
 ppath = "plugins/*.py"
 files = glob.glob(ppath)
 LazyPrincessBot.start()
 loop = asyncio.get_event_loop()
-
 
 async def Lazy_start():
     print('\n')
@@ -70,6 +67,19 @@ async def Lazy_start():
     temp.BANNED_USERS = b_users
     temp.BANNED_CHATS = b_chats
     await Media.ensure_indexes()
+    await Media2.ensure_indexes()
+    stats = await clientDB.command('dbStats')
+    free_dbSize = round(512-((stats['dataSize']/(1024*1024))+(stats['indexSize']/(1024*1024))), 2)
+    if SECONDDB_URI and free_dbSize < 10:
+        tempDict["indexDB"] = SECONDDB_URI
+        logging.info(f"Since Primary DB has only {free_dbSize} MB left, Secondary DB will be used to store data.")
+    elif SECONDDB_URI is None:
+        logging.error("Missing second DB URI! Exiting...")
+        exit()
+    else:
+        logging.info(f"Since primary DB has enough space ({free_dbSize}MB) left, it will be used for storing data.")
+    await choose_mediaDB()
+
     me = await LazyPrincessBot.get_me()
     temp.ME = me.id
     temp.U_NAME = me.username
@@ -89,10 +99,9 @@ async def Lazy_start():
     await web.TCPSite(app, bind_address, PORT).start()
     await idle()
 
-
 if __name__ == '__main__':
     try:
         loop.run_until_complete(Lazy_start())
     except KeyboardInterrupt:
         logging.info('Service Stopped Bye 👋')
-        
+    
