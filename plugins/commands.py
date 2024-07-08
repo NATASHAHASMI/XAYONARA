860cc03b861f42d0e44092c8145587045ee34869
NATASHAHASMI <150996197+NATASHAHASMI@users.noreply.github.com>
2024-07-09 01:07:43 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -82,34 +82,37 @@ async def start(client, message):
         )
         return
     
-    if AUTH_CHANNEL and not await is_subscribed(client, message):
-        try:
-            invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL))
-        except ChatAdminRequired:
-            logger.error("Make sure Bot is admin in Forcesub channel")
-            return
-        btn = [
-            [
-                InlineKeyboardButton("🍿 Jᴏɪɴ Bᴀᴄᴋᴜᴘ Cʜᴀɴɴᴇʟ 🍿", url=invite_link.invite_link)
-            ],[
-                InlineKeyboardButton("㋡ Wʜʏ l'ᴍ Jᴏɪɴɪɴɢ ❓", callback_data='sinfo')
-            ]
-        ]
-
-        if message.command[1] != "subscribe":
-            try:
-                kk, file_id = message.command[1].split("_", 1)
-                btn.append([InlineKeyboardButton("🔄 Tʀʏ Aɢᴀɪɴ", callback_data=f"checksub#{kk}#{file_id}")])
-            except (IndexError, ValueError):
-                btn.append([InlineKeyboardButton("🔄 Tʀʏ Aɢᴀɪɴ", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
-        await client.send_photo(
-            chat_id=message.from_user.id,
-            photo="https://telegra.ph/file/20b4aaaddb8aba646e53c.jpg",
-            caption="**You are not in our channel given below so you don't get the movie file...\n\nIf you want the movie file, click on the '🍿ᴊᴏɪɴ ᴏᴜʀ ʙᴀᴄᴋ-ᴜᴘ ᴄʜᴀɴɴᴇʟ🍿' button below and join our back-up channel, then click on the '🔄 Try Again' button below...\n\nThen you will get the movie files...**",
-            reply_markup=InlineKeyboardMarkup(btn),
-            parse_mode=enums.ParseMode.MARKDOWN
-            )
+    if AUTH_CHANNEL1 and AUTH_CHANNEL2 and not await is_subscribed(client, message):
+    try:
+        invite_link1 = await client.create_chat_invite_link(int(AUTH_CHANNEL1))
+        invite_link2 = await client.create_chat_invite_link(int(AUTH_CHANNEL2))
+    except ChatAdminRequired:
+        logger.error("Make sure Bot is admin in Forcesub channel")
         return
+    btn = [
+        [
+            InlineKeyboardButton("🍿 Jᴏɪɴ Oᴜʀ Cʜᴀɴɴᴇʟ 1 🍿", url=invite_link1.invite_link)
+        ],[
+            InlineKeyboardButton("🍿 Jᴏɪɴ Oᴜʀ Cʜᴀɴɴᴇʟ 2 🍿", url=invite_link2.invite_link)
+        ],[
+            InlineKeyboardButton("㋡ Wʜʏ l'ᴍ Jᴏɪɴɪɴɢ ❓", callback_data='sinfo')
+        ]
+    ]
+
+    if message.command[1] != "subscribe":
+        try:
+            kk, file_id = message.command[1].split("_", 1)
+            btn.append([InlineKeyboardButton("🔄 Tʀʏ Aɢᴀɪɴ", callback_data=f"checksub#{kk}#{file_id}")])
+        except (IndexError, ValueError):
+            btn.append([InlineKeyboardButton("🔄 Tʀʏ Aɢᴀɪɴ", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
+    await client.send_photo(
+        chat_id=message.from_user.id,
+        photo="https://telegra.ph/file/20b4aaaddb8aba646e53c.jpg",
+        caption="**You are not in our channels given below so you don't get the movie file...\n\nIf you want the movie file, click on the '🍿ᴊᴏɪɴ ᴏᴜʀ ʙᴀᴄᴋ-ᴜᴘ ᴄʜᴀɴɴᴇʟ🍿' buttons below and join our back-up channels, then click on the '🔄 Try Again' button below...\n\nThen you will get the movie files...**",
+        reply_markup=InlineKeyboardMarkup(btn),
+        parse_mode=enums.ParseMode.MARKDOWN
+    )
+    return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
                     InlineKeyboardButton('👨‍🚒 ʜᴇʟᴘ', callback_data='help'),
