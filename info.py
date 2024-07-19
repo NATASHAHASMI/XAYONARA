fab38ca8f986c4b1dfbb5f1b564dc59312f49e31
NATASHAHASMI <150996197+NATASHAHASMI@users.noreply.github.com>
2024-07-20 04:09:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -15,7 +15,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '24690581'))
 API_HASH = environ.get('API_HASH', '9f8082811c4e168f298e4574da790383')
-BOT_TOKEN = environ.get('BOT_TOKEN', "")
+BOT_TOKEN = environ.get('BOT_TOKEN', "5845533149:AAG9kBcxXvMwu7HABDlGGADjWXE2cBeluIA")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
@@ -27,8 +27,8 @@ MELCOW_IMG = environ.get("MELCOW_VID", "https://graph.org/file/411ffa270f82bac09
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1843754190').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001638006524 -1001983152794 -1001965301580 -1001638006524 -1002026546209 -1002062484191 -1001644930682 -1001756564118 -1002045252181 -1002004346552').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1843754190 5837099475').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001638006524 -1001983152794 -1001965301580 -1001638006524 -1002026546209 -1002062484191 -1001644930682 -1001756564118 -1002045252181 -1002004346552 -1002196649548').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '1843754190').split()]
@@ -43,9 +43,9 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", True))
 
 # MongoDB information
-SECONDDB_URI = environ.get('SECONDDB_URI', None)
-DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "xayonara")
+SECONDDB_URI = environ.get('SECONDDB_URI', "mongodb+srv://zonethan:unknown@cluster7.wvwc8vf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster7")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://zonethan2:unknown@cluster0.poejycb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "zonethan")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
