63a1ba94252372b79713ae95fe40c92bfda3750b
NATASHAHASMI <150996197+NATASHAHASMI@users.noreply.github.com>
2024-08-15 16:34:21 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -54,7 +54,9 @@ class temp(object):
     SETTINGS = {}
     IMDB_CAP = {}
 
-async def is_subscribed(bot, query):
+async def is_req_subscribed(bot, query):
+    if await db.find_join_req(query.from_user.id):
+        return True
     try:
         user = await bot.get_chat_member(AUTH_CHANNEL, query.from_user.id)
     except UserNotParticipant:
