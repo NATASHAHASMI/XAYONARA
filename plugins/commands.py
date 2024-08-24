9abaf5b82d122628163f1f5b23c5d6683229027b
NATASHAHASMI <150996197+NATASHAHASMI@users.noreply.github.com>
2024-08-25 03:55:43 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -13,7 +13,7 @@ from pyrogram.types import *
 from database.ia_filterdb import Media, Media2,  get_file_details, unpack_new_file_id, get_bad_files
 from database.users_chats_db import db
 from info import CHANNELS, ADMINS, AUTH_CHANNEL, LOG_CHANNEL, PICS, BATCH_FILE_CAPTION, CUSTOM_FILE_CAPTION, PROTECT_CONTENT, CHNL_LNK, GRP_LNK, REQST_CHANNEL, SUPPORT_CHAT_ID, SUPPORT_CHAT, MAX_B_TN, VERIFY, SHORTLINK_API, SHORTLINK_URL, TUTORIAL, IS_TUTORIAL, PREMIUM_USER
-from utils import get_settings, get_size, save_group_settings, temp, verify_user, check_token, check_verification, get_token, get_shortlink, get_tutorial, is_req_subscribed
+from utils import get_settings, get_size, save_group_settings, temp, verify_user, check_token, check_verification, get_token, get_shortlink, get_tutorial, is_req_subscribed, check_subscription_status
 from utils import react_msg
 from database.connections_mdb import active_connection
 # from plugins.pm_filter import ENABLE_SHORTLINK
@@ -27,10 +27,25 @@ TIMEZONE = "Asia/Kolkata"
 
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
+    user_id = message.from_user.id
+    
+    # Check if user is subscribed to the required channels
+    if not await check_subscription_status(user_id):
+        buttons = [
+            [InlineKeyboardButton('⚔️ Join Required Channels ⚔️', url="https://t.me/Moviestudioabhi")]
+        ]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await message.reply_text(
+            "Please join the required channels before using the bot. Click the button below to join:",
+            reply_markup=reply_markup
+        )
+        return
+    
     try:
         await react_msg(client, message)
     except:
         pass
+
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [[
                     InlineKeyboardButton('⤬ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ ⤬', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
@@ -41,19 +56,25 @@ async def start(client, message):
                     InlineKeyboardButton('⚔️ ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ ⚔️', url="https://t.me/Moviestudioabhi")
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        m=await message.reply_text("<b><i>ꜱᴛᴀʀᴛɪɴɢ...⚡</i></b>")
+        m = await message.reply_text("<b><i>ꜱᴛᴀʀᴛɪɴɢ...⚡</i></b>")
         await asyncio.sleep(0.5)
         await m.delete()
-        await message.reply(script.GSTART_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup, disable_web_page_preview=True)
-        await asyncio.sleep(2) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
+        await message.reply(
+            script.GSTART_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME),
+            reply_markup=reply_markup,
+            disable_web_page_preview=True
+        )
+        await asyncio.sleep(2) # Wait a bit before checking.
         if not await db.get_chat(message.chat.id):
-            total=await client.get_chat_members_count(message.chat.id)
+            total = await client.get_chat_members_count(message.chat.id)
             await client.send_message(LOG_CHANNEL, script.LOG_TEXT_G.format(message.chat.title, message.chat.id, total, "Unknown"))       
             await db.add_chat(message.chat.id, message.chat.title)
         return 
+    
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
+    
     if len(message.command) != 2:
         buttons = [[
                     InlineKeyboardButton('👨‍🚒 ʜᴇʟᴘ', callback_data='help'),
@@ -72,15 +93,15 @@ async def start(client, message):
             gtxt = "<b>ɢᴏᴏᴅ ᴇᴠᴇɴɪɴɢ 🌇</b>"
         else:
             gtxt = "<b>ɢᴏᴏᴅ ɴɪɢʜᴛ 🥱</b>"
-        m=await message.reply_text("<b><i>ꜱᴛᴀʀᴛɪɴɢ</i></b>")
+        m = await message.reply_text("<b><i>ꜱᴛᴀʀᴛɪɴɢ</i></b>")
         await asyncio.sleep(0.2)
         await m.edit_text("<b><i>ꜱᴛᴀʀᴛɪɴɢ.</i></b>")
         await asyncio.sleep(0.2)
         await m.edit_text("<b><i>ꜱᴛᴀʀᴛɪɴɢ..</i></b>")
-        await m.react("⚡",big=True)
+        await m.react("⚡", big=True)
         await asyncio.sleep(0.2)
         await m.edit_text("<b><i>ꜱᴛᴀʀᴛɪɴɢ..⚡</i></b>")
-        await m.react("😇",big=True)
+        await m.react("😇", big=True)
         await asyncio.sleep(0.2)
         await m.edit_text("⚡")
         await asyncio.sleep(0.3)
