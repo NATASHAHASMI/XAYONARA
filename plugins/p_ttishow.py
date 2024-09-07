dffe064e9ad656f369be9e395f75f37f11a08f0f
NATASHAHASMI <150996197+NATASHAHASMI@users.noreply.github.com>
2024-09-07 11:45:59 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -165,7 +165,7 @@ async def re_enable_chat(bot, message):
     await message.reply("Chat Successfully re-enabled")
 
 
-@Client.on_message(filters.command('stats') & filters.incoming)
+@Client.on_message(filters.command('stats') & filters.user(ADMINS))
 async def get_ststs(bot, message):
     rju = await message.reply(f'<i><b>Fᴇᴛᴄʜɪɴɢ Sᴛᴀᴛꜱ</b></i>')
     await asyncio.sleep(0.2)
