2089333e125cd168be856fb6cd08d57bb9fa0a54
NATASHAHASMI <150996197+NATASHAHASMI@users.noreply.github.com>
2024-09-08 16:48:44 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -160,7 +160,7 @@ Any Doubts or Not Connecting? Contact Me <a href='https://t.me/XAYOONARA'>✘ �
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-    CAPTION = """<b>🏷 Tɪᴛʟᴇ :</b><pre><b><i>{file_name}</i></b></pre>\n\n<blockquote><b>📢 Jᴏɪɴ </b></blockquote>: <b>@Moviestudioabhi</b> ❤️‍🔥\n\n<blockquote><b><i><a href='https://t.me/+mzhkyZUCrhJhZjA1'>Jᴏɪɴ 18+ Cᴏɴᴛᴇɴᴛ.🔞</a></i></b></blockquote>""" 
+    CAPTION = """<b>🏷 Tɪᴛʟᴇ :</b><pre><b><i>{file_name}</i></b></pre>\n\n<blockquote><b>📢 Jᴏɪɴ </b></blockquote>: <b>@Moviestudioabhi</b> ❤️‍🔥\n\n<blockquote><b><i><a href='https://t.me/+mzhkyZUCrhJhZjA1'>Mᴜꜱᴛ Jᴏɪɴ Dᴇᴀʟꜱ Cʜᴀɴɴᴇʟ.🏴‍☠️</a></i></b></blockquote>""" 
 
     IMDB_TEMPLATE_TXT = """
     <b>📌{title}.</b>
