9688e5846634f21fe8d9f32b592714314bba3e8b
NATASHAHASMI <150996197+NATASHAHASMI@users.noreply.github.com>
2024-09-16 18:43:09 +0530

    Update files_delete.py

--- plugins/files_delete.py
+++ plugins/files_delete.py
@@ -21,14 +21,10 @@ async def deletemultiplemedia(bot, message):
         return
 
     file_id, file_ref = unpack_new_file_id(media.file_id)
-    if await Media.count_documents({'file_id': file_id}):
-        result = await Media.collection.delete_one({
-            '_id': file_id,
-        })
-    else:
-        result = await Media2.collection.delete_one({
-            '_id': file_id,
-        })
+
+    result = await Media.collection.delete_one({
+        '_id': file_id,
+    })
     if result.deleted_count:
         logger.info('File is successfully deleted from database.')
     else:
@@ -41,29 +37,12 @@ async def deletemultiplemedia(bot, message):
         if result.deleted_count:
             logger.info('File is successfully deleted from database.')
         else:
-            result = await Media2.collection.delete_many({
-                'file_name': file_name,
+            result = await Media.collection.delete_many({
+                'file_name': media.file_name,
                 'file_size': media.file_size,
                 'mime_type': media.mime_type
             })
             if result.deleted_count:
                 logger.info('File is successfully deleted from database.')
             else:
-                result = await Media.collection.delete_many({
-                    'file_name': media.file_name,
-                    'file_size': media.file_size,
-                    'mime_type': media.mime_type
-                   })
-                if result.deleted_count:
-                    logger.info('File is successfully deleted from database.')
-                else:
-                    result = await Media2.collection.delete_many({
-                        'file_name': media.file_name,
-                        'file_size': media.file_size,
-                        'mime_type': media.mime_type
-                    })
-                    if result.deleted_count:
-                        logger.info('File is successfully deleted from database.')
-                    else:
-                        logger.info('File not found in database.')
-                        
+                logger.info('File not found in database.')
