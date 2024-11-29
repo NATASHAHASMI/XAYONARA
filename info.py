f3e2907699d9dabab3ca02036499a97d302229c9
NATASHAHASMI <150996197+NATASHAHASMI@users.noreply.github.com>
2024-11-29 21:03:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,7 +43,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", True))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://multiverse:multiverseJd6K5YKN@cluster0.uqyga.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb://xayonara:hsEGY5oR6ZHgg8V9@xayonara/?ssl=true&replicaSet=atlas-op70qy-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "multiverse")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
