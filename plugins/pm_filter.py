7c1280fd1847a0470f4147e422a01ddceffeeb64
NATASHAHASMI <150996197+NATASHAHASMI@users.noreply.github.com>
2024-11-29 20:42:19 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1393,12 +1393,12 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await query.answer(url=f"https://telegram.me/{temp.U_NAME}?start=file_{file_id}")
     
     elif query.data.startswith("checksub"):
-        if AUTH_CHANNEL and not await is_req_subscribed(client, query):
-            await query.answer("JᴏɪN Bᴀᴄᴋ-ᴜᴘ ᴄʜᴀɴɴᴇʟ ᴍᴀʜɴ! 😒", show_alert=True)
+        if AUTH_CHANNEL and not await is_req_subscribed(client, query, AUTH_CHANNEL):
+            ident, kk, file_id = query.data.split("#")
+            await query.answer(url=f"https://t.me/{temp.U_NAME}?start={kk}_{file_id}")
             return
-        ident, kk, file_id = query.data.split("#")
-        await query.answer(url=f"https://t.me/{temp.U_NAME}?start={kk}_{file_id}")
-    
+        await query.answer("Jᴏɪɴ ᴏᴜʀ Bᴀᴄᴋ-ᴜᴘ ᴄʜᴀɴɴᴇʟ ᴍᴀʜɴ! 😒", show_alert=True)
+        
     elif query.data == "pages":
         await query.answer()
     
